16452debad926104b9716b8cbde7e8f7df66ca61
VISCHENZISCH <vischenzisch@gmail.com>
2026-06-12 15:34:38 +0100

    Update PP

--- run.sh
+++ run.sh
@@ -12,8 +12,8 @@ if [ "$1" == "--install" ]; then
     echo -e "\033[38;5;46m[*] Mise à jour des dépôts système...\033[0m"
     sudo apt-get update -y
 
-    echo -e "\033[38;5;46m[*] Installation des paquets système essentiels (Tshark, SSDEEP, Volatility3)...\033[0m"
-    sudo apt-get install -y python3-pip python3-dev python3-venv build-essential libffi-dev libssl-dev ssdeep libfuzzy-dev tshark volatility3
+    echo -e "\033[38;5;46m[*] Installation des paquets système essentiels (Tshark, SSDEEP)...\033[0m"
+    sudo apt-get install -y python3-pip python3-dev python3-venv build-essential libffi-dev libssl-dev ssdeep libfuzzy-dev tshark
     
     echo -e "\033[38;5;220m[+] Prérequis système installés !\033[0m"
 fi
@@ -38,9 +38,9 @@ if [ "$1" == "--install" ]; then
     echo -e "\033[38;5;220m[+] Installation complète réussie ! Vous pouvez maintenant lancer la plateforme avec ./run.sh\033[0m"
     exit 0
 else
-    # Vérification furtive au lancement
-    pip install --disable-pip-version-check --upgrade pip -q >/dev/null 2>&1 || true
-    pip install --disable-pip-version-check -r requirements.txt -q >/dev/null 2>&1 || true
+    # Vérification au lancement
+    python3 -m pip install --disable-pip-version-check --upgrade pip -q
+    python3 -m pip install --disable-pip-version-check -r requirements.txt -q
 fi
 
 # 4. Lancement de l'application avec les arguments fournis
